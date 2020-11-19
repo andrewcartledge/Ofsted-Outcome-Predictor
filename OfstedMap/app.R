eabcfb2093b245d159a1ef53e56ef3d4c0968616
Andrew Cartledge <andrewcartledge@warwickshire.gov.uk>
2020-11-19 14:32:09 +0000

    Day 8 update 2
    App is broken again, trying to get a map working.

--- OfstedMap/app.R
+++ OfstedMap/app.R
@@ -1,24 +1,51 @@
+# Check packages are installed, probably not necessary here but just in case!
+
+if(!require("shiny")) install.packages("shiny", dependencies = TRUE)
+if(!require("shinyWidgets")) install.packages("shinyWidgets", dependencies = TRUE)
+if(!require("tidyverse")) install.packages("tidyverse", dependencies = TRUE)
+if(!require("leaflet")) install.packages("leaflet", dependencies = TRUE)
+if(!require("sp")) install.packages("sp", dependencies = TRUE)
+
+# Load packages
+
 library(shiny)
 library(shinyWidgets)
 library(tidyverse)
+library(leaflet)
+library(sp)
 
-ofsted_map <- read_csv(
+# Load in csv of data and predictions
+
+ofsted_data <- read_csv(
     "data/Inspection Outcome Map Data - Logistic Regression.csv"
     )
 
-phase_list <- unique(ofsted_map$Ofstedphase)
-outcome_list <- unique(ofsted_map$prev_good)
-max_schools <-nrow(ofsted_map)
+# Transform Easting/Northings into Lat/Longs as Leaflet will want them that way
+
+school_location <- ofsted_data %>%
+    select(Easting, Northing) %>%
+    rename(lat = Northing, long = Easting)
+
+school_data <- ofsted_data %>%
+    select(-Easting, -Northing)
+
+ofsted_map <- SpatialPointsDataFrame(
+    coords = school_location,
+    data = school_data,
+    proj4string = CRS("+init=epsg:27700")
+) %>%
+    spTransform(CRS("+init=epsg:4326"))
+
 
 ui <- fluidPage(
     sidebarLayout(
         sidebarPanel(
-            selectInput("phase", "Select school phase(s)", ofsted_map$Ofstedphase, selected = phase_list, multiple = TRUE),
-            selectInput("outcome", "Select current ofsted outcome group", ofsted_map$prev_good, selected = outcome_list, multiple = TRUE),
-            sliderInput("poorschools", "Select how many at risk schools to display", min = 0, max = max_schools, value = 20)
+            selectInput("phase", "Select school phase(s)", ofsted_map$Ofstedphase, multiple = TRUE),
+            selectInput("outcome", "Select current ofsted outcome group", ofsted_map$prev_good, multiple = TRUE),
+            numericInput("poorschools", "Enter how many at risk schools to display", value = 20)
         ),
         mainPanel(
-            textOutput("test"),
+            leafletOutput("schoolmap"),
             tableOutput("datatable")
         )
     )
@@ -37,15 +64,27 @@ server <- function(input, output, session) {
             {temp_map <- temp_map %>%
             filter(temp_map$prev_good %in% input$outcome)}
         
+        if (isTruthy(input$poorschools))
+            {temp_map <- temp_map %>%
+            arrange(desc(temp_map$bad_out_chance)) %>%
+            slice(1:input$poorschools)}
+        
     })
     
-    sliderupdate <- reactive({
-        max_schools = nrow(map_table)
-        updateSliderInput(session, "poorschools", max = max_schools)
+    map <- reactive({
+        leaflet() %>%
+        addProviderTiles(providers$OpenStreetMap) %>%
+        addMarkers(
+        data = getMapData(map_table())#,
+        # lng = map_table()$long,
+        # lat = map_table()$lat
+    )
     })
-
-    output$test <- renderText(max_schools)
+    # 
+    output$schoolmap <- renderLeaflet(map())
     output$datatable <- renderTable(map_table())
+    
+    # output$datatable <- renderTable(view(ofsted_map))
     
 }
 
