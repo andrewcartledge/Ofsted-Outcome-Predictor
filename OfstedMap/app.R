5a5626e59cf626ffb8ea74f1428d53d46c3286a4
Chris Beeley <chris.beeley@gmail.com>
2020-11-20 20:25:30 +0000

    Map fix

--- OfstedMap/app.R
+++ OfstedMap/app.R
@@ -1,5 +1,6 @@
 # Check packages are installed, probably not necessary here but just in case!
 
+if(!require("rgdal")) install.packages("sp", dependencies = TRUE)
 if(!require("shiny")) install.packages("shiny", dependencies = TRUE)
 if(!require("shinyWidgets")) install.packages("shinyWidgets", dependencies = TRUE)
 if(!require("tidyverse")) install.packages("tidyverse", dependencies = TRUE)
@@ -16,32 +17,20 @@ library(sp)
 
 # Load in csv of data and predictions
 
-ofsted_data <- read_csv(
-    "data/Inspection Outcome Map Data - Logistic Regression.csv"
-    )
-
 # Transform Easting/Northings into Lat/Longs as Leaflet will want them that way
 
-school_location <- ofsted_data %>%
-    select(Easting, Northing) %>%
+ofsted_data <- read_csv(
+    "data/Inspection Outcome Map Data - Logistic Regression.csv") %>% 
     rename(lat = Northing, long = Easting)
-
-school_data <- ofsted_data %>%
-    select(-Easting, -Northing)
-
-ofsted_map <- SpatialPointsDataFrame(
-    coords = school_location,
-    data = school_data,
-    proj4string = CRS("+init=epsg:27700")
-) %>%
-    spTransform(CRS("+init=epsg:4326"))
 
 
 ui <- fluidPage(
     sidebarLayout(
         sidebarPanel(
-            selectInput("phase", "Select school phase(s)", ofsted_map$Ofstedphase, multiple = TRUE),
-            selectInput("outcome", "Select current ofsted outcome group", ofsted_map$prev_good, multiple = TRUE),
+            selectInput("phase", "Select school phase(s)", 
+                        unique(ofsted_data$Ofstedphase), multiple = TRUE),
+            selectInput("outcome", "Select current ofsted outcome group", 
+                        unique(ofsted_map$prev_good), multiple = TRUE),
             numericInput("poorschools", "Enter how many at risk schools to display", value = 20)
         ),
         mainPanel(
@@ -53,39 +42,52 @@ ui <- fluidPage(
 
 server <- function(input, output, session) {
     
-    map_table <- reactive({
-        temp_map <- ofsted_map
+    map_data <- reactive({
         
-        if (isTruthy(input$phase))
-            {temp_map <- temp_map %>%
-            filter(temp_map$Ofstedphase %in% input$phase)}
+        temp_data <- ofsted_data
         
-        if (isTruthy(input$outcome))
-            {temp_map <- temp_map %>%
-            filter(temp_map$prev_good %in% input$outcome)}
+        if (isTruthy(input$phase)) {
+            temp_data <- temp_data %>%
+                filter(Ofstedphase %in% input$phase)
+        }
         
-        if (isTruthy(input$poorschools))
-            {temp_map <- temp_map %>%
-            arrange(desc(temp_map$bad_out_chance)) %>%
-            slice(1:input$poorschools)}
+        if (isTruthy(input$outcome)) {
+            temp_data <- temp_data %>%
+                filter(prev_good %in% input$outcome)
+        }
         
+        if (isTruthy(input$poorschools)) {
+            temp_data <- temp_data %>%
+                arrange(desc(bad_out_chance)) %>%
+                slice(1:input$poorschools)
+        }
+        
+        school_location <- temp_data %>%
+            select(long, lat)
+        
+        school_data <- temp_data %>%
+            select(-lat, -long)
+        
+        ofsted_map <- SpatialPointsDataFrame(
+            coords = school_location,
+            data = school_data,
+            proj4string = CRS("+init=epsg:27700")) %>%
+            spTransform(CRS("+init=epsg:4326")
+            )
     })
     
-    map <- reactive({
+    output$schoolmap <- renderLeaflet({
         leaflet() %>%
-        addProviderTiles(providers$OpenStreetMap) %>%
-        addMarkers(
-        data = getMapData(map_table())#,
-        # lng = map_table()$long,
-        # lat = map_table()$lat
-    )
+            addProviderTiles(providers$OpenStreetMap) %>%
+            addMarkers(
+                data = map_data()
+                # lng = map_table()$long,
+                # lat = map_table()$lat
+            )
+        
     })
-    # 
-    output$schoolmap <- renderLeaflet(map())
-    output$datatable <- renderTable(map_table())
     
-    # output$datatable <- renderTable(view(ofsted_map))
-    
+    output$datatable <- renderTable(map_data())
 }
 
 shinyApp(ui, server)
